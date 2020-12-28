823fd3efcbef97a9aa12169a7a571fb5e55cf487
MarkyMan4 <markus.hutnik@gmail.com>
2020-12-28 00:32:54 -0600

    script to add book info to database

--- DataCollection/save_to_db.py
+++ DataCollection/save_to_db.py
@@ -3,6 +3,45 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import sqlite3
+
+conn = sqlite3.connect('SkyrimLibrary.db')
+cursor = conn.cursor()
 
 df = pd.read_csv('DataCollection/books.csv', delimiter='|')
-print(df)
+
+df = df.where(pd.notnull(df), '') # replace nan with empty string
+
+# scrape book text and insert book info into Book table
+for i in range(len(df)):
+    r = requests.get(df.iloc[i]['URL'])
+    html = r.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # need to fix this later, some books' content aren't in a div with class "book"
+    # skipping over these for now
+    try:
+        # Some books start with a fancy letter that is actually an image.
+        # Need to find a way to get that letter as well so the first letter of the
+        # book isn't cut off.
+        book_content = soup.find('div', {'class': 'book'}).text
+
+        cursor.execute("""
+            INSERT INTO Book(Title, Value, Author, Description, Type, Content)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            [
+                df.iloc[i]['Title'],
+                int(df.iloc[i]['Value']), # can't use numpy data types
+                df.iloc[i]['Author'],
+                df.iloc[i]['Description'],
+                df.iloc[i]['Type'],
+                book_content
+            ]
+        )
+
+    except:
+        pass
+
+conn.commit()
+conn.close()
